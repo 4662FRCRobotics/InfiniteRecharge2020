3f067b4b1eef0edf3f410390f1e035304b6720ac
FRC4662-DS3 <bytesizedprogramming@gmail.com>
2020-03-05 15:36:29 -0800

    Increased climb speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -120,7 +120,7 @@ public final class Constants {
         public static final int kCLIMBER_FWD_PORT = 7;
         public static final int kCLIMBER_INV_PORT = 12;
         public static final int kCLIMBER_BRAKE_PORT = 1;
-        public static final double kCLIMB_SPEED = 0.2;
+        public static final double kCLIMB_SPEED = 0.8;
         public static final double kCLIMB_UP_DIRECTION = 1;
         public static final double kCLIMB_UP_SPEED = kCLIMB_SPEED * kCLIMB_UP_DIRECTION;
         public static final double kCLIMB_DOWN_SPEED = kCLIMB_SPEED * -kCLIMB_UP_DIRECTION;
